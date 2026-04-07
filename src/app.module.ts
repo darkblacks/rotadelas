0dc477df0e1dd34d484f120dcdf3c64035584418
Kauvinzera <kauvini2007@gmail.com>
2026-04-07 13:58:06 -0300

    app.module e main

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
 import { CorridaModule } from './corrida/corrida.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { Corrida } from './corrida/corrida.entity';
 
 @Module({
-  imports: [CorridaModule],
+  imports: [
+    TypeOrmModule.forRoot({ 
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'lf@gmail27',
+      database: 'db_blogpessoal',
+      entities: [Corrida],
+      synchronize: true
+    }),
+CorridaModule]
 })
 export class AppModule {}
