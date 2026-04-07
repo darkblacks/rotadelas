71dc3640c1f96bbeffe7d55659b03976d75c9640
taisduarte <tahysduarte22@gmail.com>
2026-04-07 13:49:21 -0300

    corrida.controller

--- src/corrida/corrida.controller.ts
+++ src/corrida/corrida.controller.ts
@@ -1,12 +1,45 @@
-import { Controller, Get } from '@nestjs/common';
-import { CorridaService } from './corrida.service';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put } from "@nestjs/common";
+import { CorridaService } from "./corrida.service";
+import { Corrida } from "./corrida.entity";
 
-@Controller('corrida')
+@Controller("/corridas")
 export class CorridaController {
-  constructor(private readonly corridaService: CorridaService) {}
 
-  @Get()
-  getHello(): string {
-    return this.corridaService.getHello();
-  }
+    constructor(private readonly corridaService: CorridaService) { }
+
+    @Get()
+    @HttpCode(HttpStatus.OK)
+    findAll(): Promise<Corrida[]> {
+        return this.corridaService.findAll();
+    }
+
+    @Get('/:id')
+    @HttpCode(HttpStatus.OK)
+    findById(@Param('id', ParseIntPipe) id: number): Promise<Corrida> {
+        return this.corridaService.findById(id);
+    }
+    
+    @Get('/destino/:destino')
+    @HttpCode(HttpStatus.OK)
+    findByDestino(@Param('destino') destino: string): Promise<Corrida[]> {
+        return this.corridaService.findAllByDestino(destino);
+    }
+
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    create(@Body() corrida: Corrida): Promise<Corrida> {
+        return this.corridaService.create(corrida);
+    }
+
+    @Put()
+    @HttpCode(HttpStatus.OK)
+    update(@Body() corrida: Corrida): Promise<Corrida> {
+        return this.corridaService.update(corrida);
+    }
+
+    @Delete('/:id')
+    @HttpCode(HttpStatus.NO_CONTENT)
+    async delete(@Param('id', ParseIntPipe) id: number): Promise<void> {
+        await this.corridaService.delete(id);
+    }
 }
